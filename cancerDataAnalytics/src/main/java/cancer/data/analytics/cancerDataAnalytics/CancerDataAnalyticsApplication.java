01fdf67aa78429ef4f031459b9eeb75757dfe47e
KarolDiniz <dinizkaroline923@gmail.com>
2023-06-15 20:29:32 -0300

    Create maven project: CancerDataAnalytics

---
+++ cancerDataAnalytics/src/main/java/cancer/data/analytics/cancerDataAnalytics/CancerDataAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package cancer.data.analytics.cancerDataAnalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CancerDataAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CancerDataAnalyticsApplication.class, args);
+	}
+
+}
---
+++ cancerDataAnalytics/src/test/java/cancer/data/analytics/cancerDataAnalytics/CancerDataAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package cancer.data.analytics.cancerDataAnalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CancerDataAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
