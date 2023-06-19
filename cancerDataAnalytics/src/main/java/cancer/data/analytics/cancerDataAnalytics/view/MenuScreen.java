eed1770aa8a4903750707b3131e625e77372a0ae
KarolDiniz <dinizkaroline923@gmail.com>
2023-06-18 22:50:57 -0300

    Update MenuScreen

--- cancerDataAnalytics/src/main/java/cancer/data/analytics/cancerDataAnalytics/view/MenuScreen.java
+++ cancerDataAnalytics/src/main/java/cancer/data/analytics/cancerDataAnalytics/view/MenuScreen.java
@@ -1,37 +1,50 @@
 package cancer.data.analytics.cancerDataAnalytics.view;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
+import javax.swing.*;
 import java.awt.*;
 
 public class MenuScreen {
     public static void main(String[] args) {
+
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            // If Nimbus is not available, fall back to cross-platform
+            try {
+                UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+            } catch (Exception ex) {
+                // Not worth my time
+            }
+        }
+
         JFrame frame = new JFrame("Menu");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().setBackground(new Color(173, 216, 230)); // Cor de fundo azul claro
+        frame.getContentPane().setBackground(new Color(136, 201, 187)); // Cor de fundo azul claro
 
         JPanel panel = new JPanel(new GridBagLayout());
-        panel.setBackground(new Color(135, 206, 250)); // Cor de fundo azul escuro
+        panel.setBackground(new Color(96, 144, 147)); // Cor de fundo azul escuro
 
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.fill = GridBagConstraints.HORIZONTAL;
         constraints.insets = new Insets(10, 10, 10, 10); // Espaçamento interno dos botões
 
-        JLabel titleLabel = new JLabel("Menu");
+        JLabel titleLabel = new JLabel("Cancer Data Analytics - Menu");
         titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
         titleLabel.setFont(new Font("Arial", Font.BOLD, 20));
         titleLabel.setForeground(Color.WHITE);
 
-        JButton cancerButton = new JButton("Cancer");
-        JButton gasButton = new JButton("Gas");
+        JButton cancerButton = new JButton("Cancer database");
+        JButton gasButton = new JButton("Gas database");
         JButton biButton = new JButton("BI");
-        JButton curiosidadesButton = new JButton("Curiosidades");
+        JButton curiosidadesButton = new JButton("Curiosity");
 
         Font buttonFont = new Font("Arial", Font.BOLD, 16);
-        Color buttonColor = new Color(70, 130, 180); // Cor azul escuro
+        Color buttonColor = new Color(11, 77, 70); // Cor azul escuro
         Color textColor = Color.WHITE;
 
         cancerButton.setFont(buttonFont);
