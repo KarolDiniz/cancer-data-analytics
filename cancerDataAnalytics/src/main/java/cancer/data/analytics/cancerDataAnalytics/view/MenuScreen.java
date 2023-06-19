0a684c5cbb11e2508a2c9d1f3c674d5e4d1d3e03
KarolDiniz <dinizkaroline923@gmail.com>
2023-06-18 23:47:42 -0300

    Refactoring MenuScreen

--- cancerDataAnalytics/src/main/java/cancer/data/analytics/cancerDataAnalytics/view/MenuScreen.java
+++ cancerDataAnalytics/src/main/java/cancer/data/analytics/cancerDataAnalytics/view/MenuScreen.java
@@ -24,27 +24,28 @@ public class MenuScreen {
 
         JFrame frame = new JFrame("Menu");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().setBackground(new Color(136, 201, 187)); // Cor de fundo azul claro
+        frame.getContentPane().setBackground(new Color(143, 61, 61)); // Cor de fundo azul claro
 
         JPanel panel = new JPanel(new GridBagLayout());
-        panel.setBackground(new Color(96, 144, 147)); // Cor de fundo azul escuro
+        panel.setBackground(new Color(218, 214, 214)); // Cor de fundo azul escuro
 
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.fill = GridBagConstraints.HORIZONTAL;
         constraints.insets = new Insets(10, 10, 10, 10); // Espaçamento interno dos botões
 
-        JLabel titleLabel = new JLabel("Cancer Data Analytics - Menu");
+        JLabel titleLabel = new JLabel("Cancer Data Analytics: Menu");
         titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
         titleLabel.setFont(new Font("Arial", Font.BOLD, 20));
-        titleLabel.setForeground(Color.WHITE);
+        titleLabel.setForeground(new Color(215, 104, 104));
 
         JButton cancerButton = new JButton("Cancer database");
-        JButton gasButton = new JButton("Gas database");
-        JButton biButton = new JButton("BI");
-        JButton curiosidadesButton = new JButton("Curiosity");
+        JButton gasButton = new JButton("Gas emission database");
+        JButton biButton = new JButton("BI: graphics");
+        JButton curiosidadesButton = new JButton("BI: text and curiosity");
+
 
         Font buttonFont = new Font("Arial", Font.BOLD, 16);
-        Color buttonColor = new Color(11, 77, 70); // Cor azul escuro
+        Color buttonColor = new Color(215, 104, 104); // Cor azul escuro
         Color textColor = Color.WHITE;
 
         cancerButton.setFont(buttonFont);
@@ -83,19 +84,26 @@ public class MenuScreen {
             CuriosidadesScreen.showCuriosidades();
         });
 
+        String imagePath = "C:\\Users\\carlo\\IdeaProjects\\cancer-data-analytics\\cancerDataAnalytics\\src\\main\\java\\cancer\\data\\analytics\\cancerDataAnalytics\\view\\img\\pulmoes.gif";
+        ImageIcon imageIcon = new ImageIcon(imagePath);
+        JLabel imageLabel = new JLabel(imageIcon);
+        constraints.gridy = 1;
+        constraints.gridx = 1;
+        panel.add(imageLabel, constraints);
+
         constraints.gridy = 0;
         panel.add(titleLabel, constraints);
 
-        constraints.gridy = 1;
+        constraints.gridy = 2;
         panel.add(cancerButton, constraints);
 
-        constraints.gridy = 2;
+        constraints.gridy = 3;
         panel.add(gasButton, constraints);
 
-        constraints.gridy = 3;
+        constraints.gridy = 4;
         panel.add(biButton, constraints);
 
-        constraints.gridy = 4;
+        constraints.gridy = 5;
         panel.add(curiosidadesButton, constraints);
 
         frame.add(panel);
