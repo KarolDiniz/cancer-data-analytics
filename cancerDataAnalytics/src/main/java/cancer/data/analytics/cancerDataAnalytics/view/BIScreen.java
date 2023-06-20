a129fd3d2439729fb208fb781695b442d40e9f91
KarolDiniz <dinizkaroline923@gmail.com>
2023-06-20 15:57:21 -0300

    Refactoring

--- cancerDataAnalytics/src/main/java/cancer/data/analytics/cancerDataAnalytics/view/BIScreen.java
+++ cancerDataAnalytics/src/main/java/cancer/data/analytics/cancerDataAnalytics/view/BIScreen.java
@@ -1,83 +1,23 @@
 package cancer.data.analytics.cancerDataAnalytics.view;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
-
-public class BIScreen {
-    private static int currentIndex = 0;
-    private static String[] imageFiles = { "C:\\Users\\carlo\\IdeaProjects\\cancer-data-analytics\\cancerDataAnalytics\\src\\main\\java\\cancer\\data\\analytics\\cancerDataAnalytics\\view\\img\\municipiosComMaiorMortalidadePorFaixaEtaria.png",
-            "C:\\Users\\carlo\\IdeaProjects\\cancer-data-analytics\\cancerDataAnalytics\\src\\main\\java\\cancer\\data\\analytics\\cancerDataAnalytics\\view\\img\\rankingEmissão.png",
-            "C:\\Users\\carlo\\IdeaProjects\\cancer-data-analytics\\cancerDataAnalytics\\src\\main\\java\\cancer\\data\\analytics\\cancerDataAnalytics\\view\\img\\tabagismoTaxa.png",
-            "C:\\Users\\carlo\\IdeaProjects\\cancer-data-analytics\\cancerDataAnalytics\\src\\main\\java\\cancer\\data\\analytics\\cancerDataAnalytics\\view\\img\\maioresTaxasTabagismo.png"}; // Nomes dos arquivos de imagem
 
 
-
-    public static void showBI() {
-        JFrame frame = new JFrame("Business Intelligence");
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
-
-        JPanel imagePanel = new JPanel(new FlowLayout());
-        JLabel imageLabel = new JLabel();
-        imagePanel.add(imageLabel);
-        imagePanel.setBackground(Color.WHITE);
-
-        Color buttonColor = new Color(215, 104, 104); // Cor azul escuro
-        Color textColor = Color.WHITE;
-
-        JPanel buttonPanel = new JPanel(new FlowLayout());
-        buttonPanel.setBackground(Color.WHITE);
-
-        JButton previousButton = new JButton("Anterior");
-        previousButton.setBackground(buttonColor);
-        previousButton.setForeground(textColor);
-
-        JButton nextButton = new JButton("Próximo");
-        nextButton.setBackground(buttonColor);
-        nextButton.setForeground(textColor);
-        previousButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                currentIndex--;
-                if (currentIndex < 0)
-                    currentIndex = imageFiles.length - 1;
-                showImage(imageLabel);
-            }
-        });
-
-        nextButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                currentIndex++;
-                if (currentIndex >= imageFiles.length)
-                    currentIndex = 0;
-                showImage(imageLabel);
-            }
-        });
-
-        buttonPanel.add(previousButton);
-        buttonPanel.add(nextButton);
-
-        frame.add(imagePanel, BorderLayout.CENTER);
-        frame.add(buttonPanel, BorderLayout.SOUTH);
-
-        showImage(imageLabel);
-
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+public class BIScreen extends AbstractScreen {
+    public BIScreen() {
+        imageFiles = new String[] {
+                "C:\\Users\\carlo\\IdeaProjects\\cancer-data-analytics\\cancerDataAnalytics\\src\\main\\java\\cancer\\data\\analytics\\cancerDataAnalytics\\view\\img\\municipiosComMaiorMortalidadePorFaixaEtaria.png",
+                "C:\\Users\\carlo\\IdeaProjects\\cancer-data-analytics\\cancerDataAnalytics\\src\\main\\java\\cancer\\data\\analytics\\cancerDataAnalytics\\view\\img\\rankingEmissão.png",
+                "C:\\Users\\carlo\\IdeaProjects\\cancer-data-analytics\\cancerDataAnalytics\\src\\main\\java\\cancer\\data\\analytics\\cancerDataAnalytics\\view\\img\\tabagismoTaxa.png",
+                "C:\\Users\\carlo\\IdeaProjects\\cancer-data-analytics\\cancerDataAnalytics\\src\\main\\java\\cancer\\data\\analytics\\cancerDataAnalytics\\view\\img\\maioresTaxasTabagismo.png"
+        };
     }
 
-    private static void showImage(JLabel imageLabel) {
-        String imagePath = imageFiles[currentIndex];
-        ImageIcon imageIcon = new ImageIcon(imagePath);
-        imageLabel.setIcon(imageIcon);
+    @Override
+    protected String[] getImageFiles() {
+        return imageFiles;
+    }
+
+    @Override
+    protected String getTitle() {
+        return "Business Intelligence";
     }
 }
-
