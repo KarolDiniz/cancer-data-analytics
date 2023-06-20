deeec7158886c8bef24338bba5cc19911b7698a9
KarolDiniz <dinizkaroline923@gmail.com>
2023-06-20 13:52:27 -0300

    Refactoring

--- cancerDataAnalytics/src/main/java/cancer/data/analytics/cancerDataAnalytics/view/GasDataScreen.java
+++ cancerDataAnalytics/src/main/java/cancer/data/analytics/cancerDataAnalytics/view/GasDataScreen.java
@@ -2,54 +2,22 @@ package cancer.data.analytics.cancerDataAnalytics.view;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+public class GasDataScreen extends DatabaseScreen {
 
-public class GasDataScreen {
     public static void showGasData() {
-        JFrame frame = new JFrame("Dados de Emissão de Gases");
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        GasDataScreen screen = new GasDataScreen();
+        screen.csvFile = "C:\\Users\\carlo\\IdeaProjects\\cancer-data-analytics\\cancerDataAnalytics\\src\\main\\java\\cancer\\data\\analytics\\cancerDataAnalytics\\view\\csv\\media_emissao_municipios.csv";
+        screen.loadData();
+        screen.createTable();
+    }
 
-        String csvFile = "gas_data.csv"; // Nome do arquivo CSV com os dados
-        String line;
-        String[] headers = null;
-        String[][] data = null;
-        BufferedReader br = null;
-
-        try {
-            br = new BufferedReader(new FileReader(csvFile));
-            headers = br.readLine().split(","); // Lê a primeira linha do arquivo como cabeçalho
-            int numRows = (int) br.lines().count(); // Conta o número de linhas restantes
-
-            data = new String[numRows][headers.length];
-            br.close();
-
-            br = new BufferedReader(new FileReader(csvFile));
-            br.readLine(); // Ignora o cabeçalho
-
-            int row = 0;
-            while ((line = br.readLine()) != null) {
-                data[row] = line.split(",");
-                row++;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        JTable table = new JTable(data, headers);
-        JScrollPane scrollPane = new JScrollPane(table);
-        frame.getContentPane().add(scrollPane);
-
-        frame.pack();
-        frame.setVisible(true);
+    @Override
+    protected String getTitle() {
+        return "Emission gas database";
     }
 }
